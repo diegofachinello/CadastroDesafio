5d89ca00ffb15d9cc040b9e1e52fae136dec1290
Diego F. Correa <diego.correa@senior.com.br>
2018-12-21 14:01:22 -0200

    Alterações novas

--- index.php
+++ index.php
@@ -3,10 +3,7 @@
     <head>
         <title>Tela de cadastro</title>
 
-        <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -26,13 +23,13 @@
             unset($_SESSION['message']);
         ?>
         </div>
-        <?php endif ?>
+            <?php endif; ?>
     
     <div class="container">
     <?php
-        $mysqli = new mysqli('localhost', 'user', 'pass', 'base') or die(mysqli_error($mysqli));
+        $mysqli = new mysqli('localhost', 'root', '', 'data') or die(mysqli_error($mysqli));
         $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
-        //pre_r($result->fetch_assoc());
+       
         ?>
 
        <div class="row align-items-center justify-content-center">  
@@ -41,7 +38,7 @@
             <tr>
                 <th>Nome</th>
                 <th>Localização</th>
-                <th colspan="2">Actions</th>
+                <th colspan="2">Ações</th>
             </tr>
             </thead>
 
@@ -77,18 +74,25 @@
 
         <div class="row align-items-center justify-content-center">  
             <form action="process.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
                 <div class="form-group">
                     <label>Nome</label>
-                    <input type="text" name="name" class="form-control" value="Descrição: ">
+                    <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Digite a descrição ">
                 </div>
                 <div class="form-group">
                     <label>Localização</label>
-                    <input type="text" name="location" class="form-control" value="Localização: ">
+                    <input type="text" name="location" class="form-control" value="<?php echo $location; ?>" placeholder="Digite a localização ">
                 </div>
                 <div class="form-group">
+                <?php 
+                    if ($update == true):
+                        ?>
+                          <button type="submit" class="btn btn-info" name="save">Atualizar</button>
+                    <?php else: ?>
                     <button type="submit" class="btn btn-primary" name="save">Salvar</button>
-                </div>
+                    <?php endif; ?>
+             </div>
             </form>
-        </div>   
-        </div>   
-    </body>
+           </div>   
+          </div>   
+         </body>
--- process.php
+++ process.php
@@ -2,19 +2,23 @@
 
 session_start();
 
-$mysqli = new mysqli('localhost', 'user', 'pass', 'base') or die(mysqli_error($mysqli));
+$mysqli = new mysqli('localhost', 'root', '', 'data') or die(mysqli_error($mysqli));
+
+$id = 0;
+$name = '';
+$update = '';
+$location = '';
 
 if (isset($_POST['save'])) {
     $name = $_POST['name'];
     $location = $_POST['location'];
 
-    $mysqli->query("INSERT INTO data (name, location) VALUES('name', '$location')") or
-                    die($mysqli->error);
+    $mysqli->query("INSERT INTO data (name, location) VALUES('name', '$location')") or die($mysqli->error);
 
                     $_SESSION['message'] = "Registro salvo!";
                     $_SESSION['msg_type'] = "Sucesso";
 
-                    header("location: index.php");
+                    header('location: index.php');
 }
 
 if (isset($_GET['delete'])) {
@@ -24,10 +28,30 @@ if (isset($_GET['delete'])) {
     $_SESSION['message'] = "Registro deletado!";
     $_SESSION['msg_type'] = "Perigo";
 
-    header("location: index.php");
+    header('location: index.php');
 }
 
 if (isset($_GET['edit'])) {
-    $id = $_GET['id'];
+    $id = $_GET['edit'];
+    $update = true;
     $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error());
+
+    if (count($result)>=1) {
+        $row = $result->fetch_array();
+        $name = $row['name'];
+        $location = $row['location'];
+    }
+}
+
+if (isset($_POST['update'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+
+    $mysqli->query("UPDATE data SET name = '$name', location = '$location' WHERE id = $id") or die($mysqli->error());
+
+                    $_SESSION['message'] = "Registro atualizado!";
+                    $_SESSION['msg_type'] = "Sucesso";
+
+                    header('location: index.php');
 }
